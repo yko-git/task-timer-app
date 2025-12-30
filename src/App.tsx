a9fb863a3f94a3efe63559f63d55013e87cb1dcb
yko-git <yko.iwata@gmail.com>
2025-12-31 07:01:49 +0900

    feat: AppにTimerDisplayコンポーネント読み込み

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,19 @@
 import { TaskList } from './features/tasks/components/TaskList'
+import { TimerDisplay } from './features/timer/components/TimerDisplay'
 
 function App() {
-  return <TaskList />
+  return (
+    <div style={{ display: 'flex', gap: '40px', padding: '20px' }}>
+      {/* 左側タイマー */}
+      <div style={{ flex: 1 }}>
+        <TimerDisplay />
+      </div>
+
+      {/* 右側タイマー */}
+      <div style={{ flex: 1 }}>
+        <TaskList />
+      </div>
+    </div>
+  )
 }
 export default App
