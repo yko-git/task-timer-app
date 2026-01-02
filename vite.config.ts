54bc923adda2139ebbf09db1905eb41dbfc6978d
yko-git <yko.iwata@gmail.com>
2026-01-02 11:36:16 +0900

    feat: vite.config.tsにtestを追加

--- vite.config.ts
+++ vite.config.ts
@@ -10,4 +10,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupfiles: ['./src/test/setup.ts'],
+  },
 })
