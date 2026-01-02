5bc003cd7fe847bc11598634f1cb037485f772ba
yko-git <yko.iwata@gmail.com>
2026-01-02 12:05:18 +0900

    fix: setup.tsを更新

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,9 +1,18 @@
-import { expect, afterEach } from 'vitest'
+import { expect, afterEach, beforeAll, afterAll } from 'vitest'
 import { cleanup } from '@testing-library/react'
 import * as matchers from '@testing-library/jest-dom/matchers'
+import { server } from '../mocks/server'
 
 expect.extend(matchers)
 
+// MSWサーバーをテスト前に起動
+beforeAll(() => server.listen())
+
+// 各テスト後にクリーンアップ
 afterEach(() => {
   cleanup()
+  server.resetHandlers()
 })
+
+// テスト終了後にMSWサーバーを停止
+afterAll(() => server.close())
